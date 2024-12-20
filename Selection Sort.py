2ac463d519c553a9bd2b50b4c9bfac453d81d626
SaeemSalam24 <saeemsalam123@gmail.com>
2024-12-20 23:41:44 +0530

    Add files via upload

---
+++ Selection Sort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    
+    return arr
+
+arr = [64, 25, 12, 22, 11]
+print("Original list:", arr)
+sorted_arr = selection_sort(arr)
+print("Sorted list:", sorted_arr)
